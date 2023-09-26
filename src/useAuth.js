f6623172b08045cc22d49655225e3089d227d568
drugamihai <34725600+drugamihai@users.noreply.github.com>
2023-09-26 19:08:46 +0300

    Update useAuth.js

--- src/useAuth.js
+++ src/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
+      .post("process.env.HOST/login", {
         code,
       })
       .then(res => {
@@ -28,7 +28,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3001/refresh", {
+        .post("process.env.HOST/refresh", {
           refreshToken,
         })
         .then(res => {
