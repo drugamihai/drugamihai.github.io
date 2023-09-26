65317dc5411ebaaec239e61160de833482bfbe58
drugamihai <34725600+drugamihai@users.noreply.github.com>
2023-09-26 19:18:19 +0300

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,7 @@ export default function Login() {
       className="d-flex justify-content-center align-items-center"
       style={{ minHeight: "100vh" }}
     >
+      <p>{process.env.CLIENT_HOST}</p>
       <a className="btn btn-success btn-lg" href={AUTH_URL}>
         Login With Spotify
       </a>
