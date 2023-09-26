efda2186a2565a35de5cf87ce2c3d92ca10e164f
drugamihai <34725600+drugamihai@users.noreply.github.com>
2023-09-26 19:08:18 +0300

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -28,7 +28,7 @@ export default function Dashboard({ code }) {
     if (!playingTrack) return
 
     axios
-      .get("http://localhost:3001/lyrics", {
+      .get("process.env.HOST/lyrics", {
         params: {
           track: playingTrack.title,
           artist: playingTrack.artist,
